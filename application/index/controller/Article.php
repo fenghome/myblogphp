54c031e199dde1b6cc0e07a1cec3d489ae318e39
feng_home@126.com <feng_home@126.com>
2020-03-02 10:57:12 +0800

    b

--- application/index/controller/Article.php
+++ application/index/controller/Article.php
@@ -4,11 +4,13 @@ namespace app\index\controller;
 use think\Controller;
 use app\index\model\Article as AModel;
 use app\index\model\Category;
+use think\Request;
 
 class Article extends Controller{
-  public function index(){
-    $articles = AModel::all();
-    $this->assign('articles',$articles);
+  public function index(Request $request){
+    $id = $request->param('id');
+    $article = AModel::get(['log_ID'=>$id]);
+    $this->assign('article',$article);
     $categories = Category::all();
     $this->assign('categories',$categories);
     return $this->fetch('article/article');
